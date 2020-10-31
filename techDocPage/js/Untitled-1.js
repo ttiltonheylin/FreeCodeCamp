5c7e6d27a1bc42550f50754aff49d1a523868a84
Thomas Tilton-Heylin <ttiltonheylin@gmail.com>
2020-10-31 16:06:46 -0500

    nav and landing screen css

---
+++ techDocPage/js/Untitled-1.js
@@ -0,0 +1,10 @@
+/*WHAT I GOT*/
+/*eslint-env es6*/
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
