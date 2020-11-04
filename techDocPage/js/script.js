e8273d2021c6cbffe6ffa90ef72db58c28c39420
Thomas Tilton-Heylin <ttiltonheylin@gmail.com>
2020-11-04 02:16:48 -0600

    semi working js css style swap

--- techDocPage/js/script.js
+++ techDocPage/js/script.js
@@ -9,3 +9,41 @@ navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
 
+
+//1. IDENTIFY TOGGLE ELEMENT
+let toggleSwitch = document.getElementById('theme-toggle');
+let cssSheet = document.getElementsByTagName('link')[0];
+let cssHref = cssSheet.href;
+let cssTitle = cssSheet.title;
+let headerImg = document.getElementsByTagName('img')[2];
+let headerImgSrc = headerImg.src;
+	console.log(cssSheet);
+	console.log("href: "+cssHref);
+	console.log("title: "+cssTitle);
+	console.log(headerImg);
+	console.log("img source:"+headerImgSrc);
+//2. ADD LISTENER FOR .CHECKED	
+toggleSwitch.addEventListener('change', function() {
+	if(cssTitle=='squidward'){
+		cssSheet.setAttribute('href', 'css/doodlebob.css');
+		cssSheet.setAttribute('title', 'doodlebob')
+		headerImg.setAttribute('src', 'resources/projector.png')
+		console.log(cssHref);
+		console.log(cssSheet);
+		console.log(headerImg);
+		console.log(headerImgSrc);
+	}else if (cssTitle=="doodlebob"){
+		cssSheet.setAttribute('href', 'css/squidward.css');
+		cssSheet.setAttribute('title', 'squidward')
+		headerImg.setAttribute('src', 'resources/cutout.png')
+		console.log(cssHref);
+		console.log(cssSheet);
+		console.log(headerImg);
+		console.log(headerImgSrc);	  
+	}
+});
+
+//
+//
+//
+
